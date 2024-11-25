85f3acb3e3e7072b0040cd2575d3daa7c473db8f
Daniel <danielw@missionreadyhq.com>
2024-11-26 11:36:03 +1300

    installed axios npm

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import ImageUploader from "./components/ImageUploader/ImageUploader";
 
 function App() {
   return (
     <>
       <h1>TurnersCars Insurance AI</h1>
+      <ImageUploader />
     </>
   );
 }
