5e1e7a83081ae21b070f5115ee52477cc7553af1
Daniel <danielw@missionreadyhq.com>
2024-11-26 11:31:52 +1300

    folder layout and basic setup for ImageUploader component

---
+++ src/components/ImageUploader/ImageUploader.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function ImageUploader() {
+  return (
+    <div>
+      <h1>ImageUploader for AI Scanning using Azure Custom Vision</h1>
+    </div>
+  );
+}
